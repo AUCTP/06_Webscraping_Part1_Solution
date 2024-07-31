0a27a6a99d3bc7881dd9c9495929026e2bbf84b3
Nikolai Stein <nikolai.stein86@gmail.com>
2024-07-31 13:53:28 +0000

    Update 01_Predict

--- 01_PredictAndRun/task1.py
+++ 01_PredictAndRun/task1.py
@@ -1,8 +1,7 @@
-import requests
-import bs4
+import requests # Import Requests library to send http requests
 
-url = 'http://www.scrapethissite.com/pages/simple/'
+url = 'http://www.scrapethissite.com/pages/simple/' # Define URL to scrape
 
-r = requests.get(url)
-print(r.status_code)
-print(r.text)
+r = requests.get(url) # Send a get request to the URL
+print(r.status_code) # Show the http status code (200: Ok, everything else: something went wrong)
+print(r.text) # Show the raw HTML returned by the webserver
--- 01_PredictAndRun/task2.py
+++ 01_PredictAndRun/task2.py
@@ -1,10 +1,10 @@
-import requests
-import bs4
+import requests # Import Requests library to send http requests
+import bs4 # Import BeautifulSoup4 library to parse HTML data
 
-url = 'http://www.scrapethissite.com/pages/simple/'
+url = 'http://www.scrapethissite.com/pages/simple/' # Define URL to scrape
 
-r = requests.get(url)
+r = requests.get(url) # Send a get request to the URL
 
-htmlDocument = bs4.BeautifulSoup(r.text, 'html.parser')
+htmlDocument = bs4.BeautifulSoup(r.text, 'html.parser') # Parse the returned HTML string to Python
 
-print(htmlDocument.find("h3").text)
+print(htmlDocument.find("h3").text) # Find the first h3 HTML document and return the text
